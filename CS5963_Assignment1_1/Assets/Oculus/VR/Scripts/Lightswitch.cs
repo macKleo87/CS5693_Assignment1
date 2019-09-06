1b0477dcaa4f11a4b32d0e7561357e965bdf33b5
Leo <u0986505@utah.edu>
2019-09-05 21:47:03 -0600

    Made first few scripts

---
+++ CS5963_Assignment1_1/Assets/Oculus/VR/Scripts/Lightswitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightswitch : MonoBehaviour {
+
+    public Light light;
+
+	// Use this for initialization
+	void Start () {
+        light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("tab"))
+        {
+            if (light.color == Color.white)
+                light.color = Color.red;
+            else
+                light.color = Color.white;
+        }
+	}
+}
