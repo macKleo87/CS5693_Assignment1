382b2cb43176f1df061c8ef983d31a27e37ccb15
Leo <u0986505@utah.edu>
2019-09-09 16:28:57 -0600

    Added xbox movement

---
+++ CS5963_Assignment1_2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+  
+ using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+
+{
+
+    private Vector3 movementVector;
+
+    private CharacterController characterController;
+
+    private float movementSpeed = 8;
+
+    private float jumpPower = 15;
+
+    private float gravity = 40;
+
+    void Start()
+
+    {
+
+        characterController = GetComponent<CharacterController>();
+
+    }
+
+    void Update()
+
+    {
+
+        movementVector.x = Input.GetAxis("LeftJoystickX") * movementSpeed;
+
+        movementVector.z = Input.GetAxis("LeftJoystickY") * movementSpeed;
+    }
+}
